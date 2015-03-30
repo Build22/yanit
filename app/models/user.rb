addf7db2b3cf9e07afd3102969bce9cba1fa91a5
Nagaraja Jade <zbdcqssd6y@snkmail.com>
2015-03-30 14:26:41 +0100

    add extjs to version control

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :issues, :foreign_key => "assignee_id"
-  
+
   scope :assignees_for_project, lambda { |project| select("DISTINCT users.*").joins(:issues).where(:issues => {:project_id => project.id}) }
 
   # netzke_attribute :number_of_issues
